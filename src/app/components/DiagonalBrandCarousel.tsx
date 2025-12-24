1f98b61da8423b89807cb5a2c90a7a58a0231367
xavier77dev <xavier.basurto.77@gmail.com>
2025-12-24 17:40:44 -0500

    Refactor code structure for improved readability and maintainability

--- src/app/components/DiagonalBrandCarousel.tsx
+++ src/app/components/DiagonalBrandCarousel.tsx
@@ -3,33 +3,37 @@
 import React from "react";
 import BrandCarousel from "./BrandCarousel";
 
-const brands1 = [
+const brands = [
   "/adiel.jpg",
   "/movento.jpg",
   "/nomadas.jpg",
   "/paraiso.jpg",
-  "/enai.jpg",
-  "/business.jpg",
+  "/logo-atenas.png",
+  "/Logo-RSV-sin-fondo.png",
   "/horse.jpg",
   "/np-products.jpg",
 ];
-const brands2 = [
-  "/adiel.jpg",
-  "/movento.jpg",
+
+const brandsRandom = [
   "/nomadas.jpg",
   "/paraiso.jpg",
-  "/enai.jpg",
-  "/business.jpg",
+  "/logo-atenas.png",
+  "/Logo-RSV-sin-fondo.png",
   "/horse.jpg",
   "/np-products.jpg",
+  "/adiel.jpg",
+  "/movento.jpg",
 ];
+
+const randomBrands = brands.sort();
+console.log(randomBrands, "randomBrands");
 
 const DiagonalBrandCarousel: React.FC = () => {
   return (
     <div className="w-full overflow-hidden relative bg-blue-dark py-16">
       <div className="transform -skew-y-3">
-        <BrandCarousel brands={brands1} />
-        <BrandCarousel brands={brands2} repeatType="reverse" />
+        <BrandCarousel brands={brands} />
+        <BrandCarousel brands={brandsRandom} repeatType="reverse" />
       </div>
     </div>
   );
