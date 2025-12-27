5cee73f0bb142145befd2c0f125e463ae50fb185
xavier77dev <xavier.basurto.77@gmail.com>
2025-12-27 16:30:52 -0500

    refactor(WrapperModal): simplify modal structure and remove unused props

--- src/components/ui/WrapperModal.tsx
+++ src/components/ui/WrapperModal.tsx
@@ -3,37 +3,26 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
-import { IconButton } from "@mui/material";
 import { Close } from "@mui/icons-material";
 
 const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
-  p: 4,
+  position: "relative",
 };
 
 interface WrapperModalProps {
   open: boolean;
   onOpen: (open: boolean) => void;
   children: React.ReactNode;
-  width?: string;
-  height?: string;
 }
 
-// jsdoc
 /**
  * WrapperModal component to display content in a modal dialog.
  * @param {boolean} open - Boolean to control the visibility of the modal.
  * @param {function} onOpen - Function to toggle the modal's open state.
  * @param {React.ReactNode} children - The content to be displayed inside the modal.
- * @param {number} [width] - Optional width of the modal in viewport width units.
- * @param {number} [height] - Optional height of the modal in viewport height units.
  * @return {JSX.Element} The WrapperModal component.
  */
 
@@ -41,8 +30,6 @@ export default function WrapperModal({
   open,
   onOpen,
   children,
-  width,
-  height,
 }: WrapperModalProps) {
   return (
     <div className="absolute">
@@ -52,6 +39,11 @@ export default function WrapperModal({
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
         closeAfterTransition
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
         slotProps={{
           backdrop: {
             sx: {
@@ -67,21 +59,15 @@ export default function WrapperModal({
             border: "2px solid #10B981",
             borderRadius: "8px",
             backgroundColor: "#FFFFFF",
-            width: `${width ?? ""}`,
-            height: `${height ?? ""}`,
-            margin: "0",
-            padding: "0",
           }}
         >
-          {/* CLOSE ICON (FUERA DEL SCROLL) */}
-          <div
-            onClick={() => onOpen(false)}
-            className="absolute top-2 right-3 sm:right-[-90px] text-gray-500 cursor-pointer"
-          >
-            <Close />
-          </div>
-
-          <div className="h-screen overflow-y-auto scrollbar-custom sm:w-[500px] bg-white">
+          <div className="relative h-[750px] max-h-screen overflow-y-auto scrollbar-custom sm:w-[500px] bg-white rounded-lg py-4 sm:py-1">
+            <div
+              onClick={() => onOpen(false)}
+              className="absolute top-4 right-3 text-gray-500 cursor-pointer"
+            >
+              <Close />
+            </div>
             {children}
           </div>
         </Box>
